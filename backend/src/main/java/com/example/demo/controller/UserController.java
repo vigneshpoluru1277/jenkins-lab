61bfb4ced24e9ddcfdf1d25d7af469ce8eec5fd2
vignesh <vigneshpoluru1277@gmail.com>
2025-10-15 19:32:36 +0530

    Updated

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/api")
+@RequestMapping("/api/")
 public class UserController {
 
     @Autowired
