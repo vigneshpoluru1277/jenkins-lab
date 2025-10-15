2680de2163d0389fc12fb173e134b9eea681516a
vignesh <vigneshpoluru1277@gmail.com>
2025-10-15 19:40:52 +0530

    Updated

--- backend/src/main/java/com/example/demo/ArtgalleryApplication.java
+++ backend/src/main/java/com/example/demo/ArtgalleryApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ArtgalleryApplication {
+public class ArtgalleryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtgalleryApplication.class, args);
